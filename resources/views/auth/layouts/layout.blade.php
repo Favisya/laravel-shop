10f923e9b06b6c943305ec11dfdb9823ce6221fc
Dmitriy Vikulin <dvikulin@lachestry.com>
2022-12-10 21:53:20 +0300

    Fix person order list
    [#875]

--- resources/views/auth/layouts/layout.blade.php
+++ resources/views/auth/layouts/layout.blade.php
@@ -54,7 +54,7 @@
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                                data-toggle="dropdown"
                                aria-haspopup="true" aria-expanded="false" v-pre>
-
+                                @admin Администратор @else {{ Auth::user()->name }} @endadmin
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
